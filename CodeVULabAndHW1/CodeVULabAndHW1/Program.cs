c0c49488b48c2e96a3216d9c108cd4b0d6172d67
Jonathan Downs <Jonathan.Downs@veteransunited.com>
2022-09-16 16:31:18 -0500

    Made a cool VS Project

---
+++ CodeVULabAndHW1/CodeVULabAndHW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeVULabAndHW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
